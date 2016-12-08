90abfbc917451ccaa396f989caf8bd4f4eaf2ca0
Victor Zhong <viczhong@gmail.com>
2016-12-08 15:47:25 -0500

    I'm Done

---
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/DetailViewController.swift
@@ -0,0 +1,79 @@
+//
+//  DetailViewController.swift
+//  AC3.2-MidtermElements
+//
+//  Created by Victor Zhong on 12/8/16.
+//  Copyright © 2016 Victor Zhong. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var element: Element?
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var meltLabel: UILabel!
+    @IBOutlet weak var boilLabel: UILabel!
+    @IBOutlet weak var stealthLabel: UILabel!
+    @IBOutlet weak var faveButton: UIButton!
+    
+    @IBAction func faveButtonTapped(_ sender: UIButton) {
+        guard let validElement = element else { return }
+        
+        let favorite: [String : Any] = [
+            "my_name" : "Vic Zhong",
+            "favorite_element" : validElement.symbol
+        ]
+        
+        APIRequestManager.manager.postRequest(endPoint: "https://api.fieldbook.com/v1/58488d40b3e2ba03002df662/favorites", data: favorite)
+        
+        self.faveButton.isEnabled = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let validElement = element {
+            self.title = validElement.name
+            loadData()
+        }
+    }
+    
+    func loadData() {
+        guard let validElement = element else { return }
+        
+        self.symbolLabel.text = String(validElement.number)
+        self.nameLabel.text = "\(validElement.name) (\(validElement.symbol))"
+        self.weightLabel.text = "Atomic Weight: \(validElement.weight)"
+        
+        if validElement.boiling == 9000 {
+            self.boilLabel.text = "Boiling Point: IT'S OVER 9,000!*"
+        }
+        else {
+            self.boilLabel.text = "Boiling Point: \(validElement.boiling)° C"
+        }
+        
+        if validElement.melting == -9000 {
+            self.meltLabel.text = "Melting Point: IT'S UNDER -9,000!*"
+        }
+        else {
+            self.meltLabel.text = "Melting Point: \(validElement.melting)° C"
+        }
+        
+        if validElement.boiling == 9000 || validElement.melting == -9000 {
+            self.stealthLabel.isHidden = false
+        }
+        
+        APIRequestManager.manager.getData(endPoint: validElement.image) { (data: Data?) in
+            if let validData = data,
+                let validImage = UIImage(data: validData) {
+                print(validElement.image)
+                DispatchQueue.main.sync {
+                    self.imageView.image = validImage
+                }
+            }
+        }
+    }
+}
---
+++ AC3.2-MidtermElements/AC3.2-MidtermElements/ElementsTableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ElementsTableViewController.swift
+//  AC3.2-MidtermElements
+//
+//  Created by Victor Zhong on 12/8/16.
+//  Copyright © 2016 Victor Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ElementsTableViewController: UITableViewController {
+    
+    let cellIdentifier = "elementCellReuse"
+    let cellSegue = "elementSegue"
+    var elements = [Element]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+    }
+    
+    func loadData() {
+        APIRequestManager.manager.getData(endPoint: "https://api.fieldbook.com/v1/58488d40b3e2ba03002df662/elements") { (data: Data?) in
+            if  let validData = data,
+                let validElements = Element.elements(from: validData) {
+                print("We have elements! \(validElements.count)")
+                self.elements = validElements
+                DispatchQueue.main.async {
+                    self.tableView?.reloadData()
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cellElement = elements[indexPath.row]
+        
+        cell.textLabel?.text = cellElement.name
+        cell.detailTextLabel?.text = "\(cellElement.symbol)(\(cellElement.number)) \(cellElement.weight)"
+        
+        APIRequestManager.manager.getData(endPoint: cellElement.thumb) { (data: Data?) in
+            if let validData = data,
+                let validImage = UIImage(data: validData) {
+                DispatchQueue.main.async {
+                    cell.imageView?.image = validImage
+                    cell.setNeedsLayout()
+                }
+            }
+        }
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let detailView = segue.destination as? DetailViewController,
+            let cell = sender as? UITableViewCell,
+            let indexPath = tableView.indexPath(for: cell) {
+            detailView.element = elements[indexPath.row]
+        }
+    }
+}
